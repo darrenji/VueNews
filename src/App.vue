ea00d728c2ab412b7413b1a57ac0f5c450c867f0
darren <764190362@qq.com>
2017-10-26 12:32:39 +0800

    vue-resource,显示列表，应对select变化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <source-selection v-on:sourceChanged="sourceChanged"></source-selection>
+        <news-list v-bind:source="source"></news-list>
+   
+    </div>
+</template>
+
+<script>
+    import SourceSelection from './components/SourceSelection.vue'
+    import Newslist from './components/Newslist.vue'
+    
+    import Test from './components/Test.vue'
+   
+    export default {
+      data () {
+        return {
+          source: ""
+        }
+      },
+      components: {
+          newsList: Newslist,
+          sourceSelection: SourceSelection,
+          appTest:Test
+      },
+      methods: {
+          sourceChanged: function(source){
+              this.source = source;
+          }
+      }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>test</p>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        
+    }
+</script>
